b0450ed381632b393adef664e638ba57b2fc8df6
xurxo <xurxof@gmail.com>
2014-10-22 22:59:17 +0200

    Better html template. Unicode support

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import codecs
 import json
 from xmarks2html import BookTree
 from jinja2 import Environment, PackageLoader
 
+
+class Data:
+    pass
+
+
+def GetData(args):
+
+    data = Data()
+    data.Type = args['ntype']
+    if 'url' in args:
+        data.Url = args['url']
+    else:
+        data.Url=''
+
+    data.Name = args.get('name', data.Url)
+
+    return data
+
+
 def main():
-    with open('bookmarks.json') as bookmarksFile:
+    with codecs.open('bookmarks.json', encoding='utf-8') as bookmarksFile:
         data = json.load(bookmarksFile)
 
     book_tree = BookTree()
     for command in data['commands']:
         if command['action'] == 'insert':
             args = command['args']
-            book_tree.Add(command['nid'], args['pnid'],
-                          {'type': args['ntype'], 'url': args.get('url', None)})
+            book_tree.Add(command['nid'], args['pnid'], GetData(args))
 
     env = Environment(loader=PackageLoader('xmarks2html', ''))
     template = env.get_template('bookmarks.template')
-    print template.render(tree=book_tree)
+    print template.render(tree=book_tree ).encode('utf-8')
 
 
 if __name__ == "__main__":
